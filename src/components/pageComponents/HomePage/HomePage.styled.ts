90e4411c820a2dd93a12728cbc88b9586f57a161
DmanRy09 <42903289@mynwu.ac.za>
2025-08-27 15:14:14 +0200

    d

--- src/components/pageComponents/HomePage/HomePage.styled.ts
+++ src/components/pageComponents/HomePage/HomePage.styled.ts
@@ -4,7 +4,7 @@ export const HomePageWrapper = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-  background-color: #f9fafb; /* same as Tailwind gray-50 */
+  background-color: #0875e2ff; /* same as Tailwind gray-50 */
   align-items: center;
   justify-content: center;
   padding: 2rem;
--- src/components/pageComponents/loginPage/loginPage.styled.ts
+++ src/components/pageComponents/loginPage/loginPage.styled.ts
@@ -4,7 +4,7 @@ export const LoginPageWrapper = styled.div`
 min-height: 100vh;
   display: flex;
   flex-direction: column;
-  background-color: #f9fafb; /* same as Tailwind gray-50 */
+  background-color: #0b7ae9ff; /* same as Tailwind gray-50 */
   align-items: center;
   justify-content: center;
   padding: 2rem;
