1f7b263f519f63fb7b6cb0ff2d82895aacc53c2a
DmanRy09 <42903289@mynwu.ac.za>
2025-08-27 14:20:48 +0200

    d

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
- 
+
 
 // Importing your components
 import Home from "./components/pageComponents/HomePage/home-page";
