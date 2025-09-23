33c243227d06d9795b9ec8b204ee4ef8c4c757e5
Jean-Pierre Retief <jpretief84@gmail.com>
2025-09-23 17:32:35 +0200

    Fix Login API

--- src/components/pageComponents/AuthPage/LoginPage.js
+++ src/components/pageComponents/AuthPage/LoginPage.js
@@ -31,7 +31,7 @@ const LoginPage = ({ setUser }) => { // <-- pass setUser from App.js
       setLoading(true);
       setError("");
 
-      const res = await api.post("api/token/", { email: email, password: password });
+      const res = await api.post("/token/", { email: email, password: password });
 
       // Save tokens + user info including role
       const user = res.data.user || { email, role: "user" }; 
