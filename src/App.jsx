8000417f9d55f86a4d507bf6a948e739c283516d
Phetogo Maloka <malokaserake@gmail.com>
2025-09-30 00:43:49 +0200

    My Culture page (specific videos yet to be embedded)

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import MyCulturePage from './pages/MyCulturePage';
+
+// Importing your components
+import Home from "./components/pageComponents/home-page";
+import Header from './components/sectionComponents/header';
+import Footer from './components/sectionComponents/footer';
+import CulturePage from './components/sectionComponents/CulturePage';
+ 
+
+function App() {
+  return (
+      <Router>
+
+        <div className="App">
+          <div className="header">
+            <Header />
+          </div>
+
+          <div className="body">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/culture" element={<CulturePage />} />
+              </Routes>
+          </div>
+
+          <div className="footer">
+            <Footer />
+          </div>
+        </div>
+      </Router>
+  );
+}
+
+export default App;
