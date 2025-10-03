73661bdfff689b170f664f3191bab2d15f2bb885
Phetogo Maloka <malokaserake@gmail.com>
2025-10-04 01:37:41 +0200

    Created WeNeed page

--- src/App.js
+++ src/App.js
@@ -8,29 +8,20 @@ import {
   useNavigate,
 } from "react-router-dom";
 import "./App.css";
-
-// Section components
 import Header from "./components/sectionComponents/header.js";
 import SiteHeader from "./components/sectionComponents/SiteHeader.js";
 import Footer from "./components/sectionComponents/footer.js";
-
-// Page components
 import HomePage from "./components/pageComponents/HomePage/home-page.js";
-import LoginPage from "./components/pageComponents/AuthPage/LoginPage.js";  
+import LoginPage from "./components/pageComponents/AuthPage/LoginPage.js";
 import RegisterPage from "./components/pageComponents/AuthPage/RegisterPage.js";
 import AboutPage from "./components/pageComponents/AboutPage/AboutPage.js";
-import MyCulturePage from "./components/pageComponents/MyCulturePage/MyCulturePage.js";
-import CampusMapPage from "./components/pageComponents/CampusMapPage/CampusMapPage.js";
-import CommunicationSkillsPage from "./components/pageComponents/CommunicationSkillsPage/CommunicationSkillsPage.js"; 
-
-//import ProtectedRoute from "./components/ProtectedRoute.js";
+import WeNeedPage from './pages/WeNeed-page';
 import { AppWrapper } from "./components/pageComponents/AppWrapper.styled.js";
 
 function Layout({ children, user, onLogout }) {
   const location = useLocation();
   const simpleHeaderPaths = ["/", "/login", "/register"];
   const useSimpleHeader = simpleHeaderPaths.includes(location.pathname);
-
   const backgroundClass = simpleHeaderPaths.includes(location.pathname)
     ? "with-background"
     : "no-background";
@@ -56,8 +47,6 @@ function App() {
     if (storedUser) {
       const userData = JSON.parse(storedUser);
       setUser(userData);
-
-      // userData.is_staff = true; // Uncomment to test staff view
     }
   }, []);
 
@@ -71,29 +60,15 @@ function App() {
     navigate("/login");
   };
 
-  //const isStaffUser =
-   // user && (user.role === "staff" || user.is_staff || user.isModerator);
-
   return (
     <AppWrapper>
       <Layout user={user} onLogout={handleLogout}>
         <Routes>
-          {/* Public routes */}
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage setUser={setUser} />} />
-          <Route path="/register" element={<RegisterPage setUser={setUser} />}/>
-          <Route path="/communicationskills" element={<CommunicationSkillsPage  />}/>
-          <Route path="/about" element={<AboutPage />} /> {/*for testing purposes to skip login part*/}
-
-          {/* MyCulture page (staff vs student) */}
-          <Route
-            path="/myculture"
-            element={<MyCulturePage user={user} />
-            }
-          />
-
-          {/* ✅ New Campus Map page */}
-          <Route path="/campusmap" element={<CampusMapPage />} />
+          <Route path="/register" element={<RegisterPage setUser={setUser} />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/weneed" element={<WeNeedPage />} />
         </Routes>
       </Layout>
     </AppWrapper>
