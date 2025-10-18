caa3d5b92ec1905a6730fb4464a0baecaf51f41d
Phetogo Maloka <malokaserake@gmail.com>
2025-10-18 15:46:12 +0200

    Feature: Implement Google Analytics tracking.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
 import React from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import PageViewTracker from "./analytics/PageViewTracker";
 
 // Importing your components
 import Home from "./components/pageComponents/home-page";
-import Header from './components/sectionComponents/header';
-import Footer from './components/sectionComponents/footer';
+import Header from "./components/sectionComponents/header";
+import Footer from "./components/sectionComponents/footer";
 
 function App() {
   return (
-      <Router>
+    <Router>
+      {/* Mount once so GA fires a page_view whenever the route changes */}
+      <PageViewTracker />
 
-        <div className="App">
-          <div className="header">
-            <Header />
-          </div>
-
-          <div className="body">
-              <Routes>
-                <Route path="/" element={<Home />} />
-              </Routes>
-          </div>
-
-          <div className="footer">
-            <Footer />
-          </div>
+      <div className="App">
+        <div className="header">
+          <Header />
         </div>
-      </Router>
+
+        <div className="body">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            {/* add more routes as needed, e.g.:
+                <Route path="/about" element={<About />} />
+                <Route path="/contact" element={<Contact />} />
+            */}
+          </Routes>
+        </div>
+
+        <div className="footer">
+          <Footer />
+        </div>
+      </div>
+    </Router>
   );
 }
 
